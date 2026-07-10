ad7c59bfb67d65bab7e50417eb2273c8549c5f37
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 20:16:26 +0400

    Update charm-validator.module.ts

--- src/charm-validator/charm-validator.module.ts
+++ src/charm-validator/charm-validator.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CharmValidatorService } from './charm-validator.service';
 import { CharmValidatorController } from './charm-validator.controller';
+import { CharmIngestionModule } from '../charm-ingestion/charm-ingestion.module';
+import { CharmCommonModule } from '../charm-common/charm-common.module';
 
 @Module({
+  imports: [CharmIngestionModule, CharmCommonModule],
   controllers: [CharmValidatorController],
   providers: [CharmValidatorService],
   exports: [CharmValidatorService]
