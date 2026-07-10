114bd939f2abeb0a3e9997e16456094a1368e7bd
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 20:31:27 +0400

    Update utils.ts

--- src/charm-common/utils.ts
+++ src/charm-common/utils.ts
@@ -6,3 +6,15 @@ export function charmLog(message: string) {
 export function toChecksum(address: string) {
   return address.trim().toLowerCase();
 }
+
+export function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function safeJSON(input: any) {
+  try {
+    return JSON.stringify(input, null, 2);
+  } catch {
+    return '{}';
+  }
+}
