a1efa3f861e7fabc7cf74f2d06664ca323f9d73f
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 20:17:16 +0400

    Update charm-validator.service.ts

--- src/charm-validator/charm-validator.service.ts
+++ src/charm-validator/charm-validator.service.ts
@@ -1,20 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CharmIngestionService } from '../charm-ingestion/charm-ingestion.service';
+import { charmLog } from '../charm-common/utils';
+import { SUPPORTED_CHAINS } from '../charm-common/constants';
 
 @Injectable()
 export class CharmValidatorService {
-  getAllValidators() {
+  private readonly logger = new Logger(CharmValidatorService.name);
+
+  constructor(
+    private readonly ingestion: CharmIngestionService
+  ) {}
+
+  async getAllValidators() {
+    charmLog('Fetching all validators');
     return { message: 'All validators fetched (Charm_Capsule)' };
   }
 
-  getValidatorById(id: string) {
+  async getValidatorById(id: string) {
+    charmLog(`Fetching validator ${id}`);
     return { message: `Validator ${id} fetched (Charm_Capsule)` };
   }
 
-  createValidator(data: any) {
-    return { message: 'Validator created (Charm_Capsule)', payload: data };
+  async createValidator(data: any) {
+    charmLog('Creating validator');
+
+    // Auto-ingest validator data from chain
+    if (SUPPORTED_CHAINS.includes(data.network)) {
+      await this.ingestion.ingestValidatorData(data.address);
+    }
+
+    return {
+      message: 'Validator created (Charm_Capsule)',
+      payload: data
+    };
   }
 
-  updateValidator(id: string, data: any) {
-    return { message: `Validator ${id} updated (Charm_Capsule)`, payload: data };
+  async updateValidator(id: string, data: any) {
+    charmLog(`Updating validator ${id}`);
+
+    return {
+      message: `Validator ${id} updated (Charm_Capsule)`,
+      payload: data
+    };
+  }
+
+  async syncValidatorFromChain(address: string, chain: string) {
+    charmLog(`Syncing validator ${address} from ${chain}`);
+
+    if (!SUPPORTED_CHAINS.includes(chain)) {
+      return {
+        error: true,
+        message: `Chain ${chain} not supported (Charm_Capsule)`
+      };
+    }
+
+    return this.ingestion.ingestValidatorData(address);
   }
 }
