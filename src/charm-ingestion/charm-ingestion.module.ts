0deaa6945bb3394aa256831e59874f0142aa3bd1
CharmCapsule <mondjoe007@yahoo.com>
2026-07-10 20:21:55 +0400

    Update charm-ingestion.module.ts

--- src/charm-ingestion/charm-ingestion.module.ts
+++ src/charm-ingestion/charm-ingestion.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CharmIngestionService } from './charm-ingestion.service';
+import { CharmCommonModule } from '../charm-common/charm-common.module';
 
 @Module({
+  imports: [CharmCommonModule],
   providers: [CharmIngestionService],
   exports: [CharmIngestionService]
 })
